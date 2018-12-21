9da9b2d85dc41292f9f403a83ee64f8147e4798f
Aleksandar Kalabić <akalabic14@raf.edu.rs>
2018-12-21 15:43:37 +0100

    fix

--- index.js
+++ index.js
@@ -6,28 +6,23 @@ module.exports = (data) => {
             nextUrls: [],
             exitCode: 0
         }
-        psql.connect()
-        .then(res => {
-            let nextUrls = html.match(/href="(\/[a-zA-Z1-9\/\-]*)"/g) ? html.match(/href="(\/[a-zA-Z1-9\/\-]*)"/g).map(href => href.match(/\/[a-zA-Z1-9\/\-]*/g)[0]).map(href => `${domain}${href.slice(1)}`) :$
-            nextUrls = [...new Set(nextUrls)];
-            result.nextUrls = nextUrls;
-            return html.toLowerCase().match(/vucic|vučić/gi) ? html.toLowerCase().match(/vucic|vučić/gi).length : 0;
-        })
-        .then(ponavljanja => {
-            if (ponavljanja === 0) {
-                return false;
-            } else {
-                return results.push(`Found vučić ${ponavljanja} times on ${page_url}`)
-            }
-        })
-        .then(res => {
-            resolve(result);
-        })
-        .catch(err => {
-            logger.error(`Error analising html. ${err.toString()}`);
-            resolve(Object.assign(result, {
-                exitCode: 1
-            }));
-        })
+        let nextUrls = html.match(/href="(\/[a-zA-Z1-9\/\-]*)"/g) ? html.match(/href="(\/[a-zA-Z1-9\/\-]*)"/g).map(href => href.match(/\/[a-zA-Z1-9\/\-]*/g)[0]).map(href => `${domain}${href.slice(1)}`) :$
+        nextUrls = [...new Set(nextUrls)];
+        result.nextUrls = nextUrls;
+        let ponavljanja = html.toLowerCase().match(/vucic|vučić/gi) ? html.toLowerCase().match(/vucic|vučić/gi).length : 0;
+        if (ponavljanja) {
+            results.push(`Found vučić ${ponavljanja} times on ${page_url}`)
+            .then(res => {
+                resolve(result);
+            })
+            .catch(err => {
+                logger.error(`Error analising html. ${err.toString()}`);
+                resolve(Object.assign(result, {
+                    exitCode: 1
+                }));
+            })
+        } else {
+            resolve(result)
+        }
     })
 }
